3468dbf057c6ba68882190d264644e9411722fc6
BrendanCheong <brendancej82@gmail.com>
2022-02-21 21:24:38 +0800

    completed arcade basketball

--- Kattis Extra/Competitive Arcade Basketball/competitive_arcade_basketball.cpp
+++ Kattis Extra/Competitive Arcade Basketball/competitive_arcade_basketball.cpp
@@ -17,6 +17,30 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
+    ll n, p, m; cin >> n >> p >> m;
+    bool winner = false;
+    unordered_map<string, ll> players;
+    while (n--) {
+        string name; cin >> name;
+        players[name] = 0;
+    }
+
+    for (size_t i = 0; i < m; i++) {
+        string player; cin >> player;
+        ll points; cin >> points;
+        if (players.count(player)) {
+            players[player] += points;
+            if (players[player] >= p) {
+                cout << player << " wins!" << endl;
+                players.erase(player);
+                winner = true;
+            }
+            //cout << player << ": " << players[player] << endl;
+        }
+    }
+
+    if (!winner)
+        cout << "No winner!" << endl;
 
     return 0;
 }
