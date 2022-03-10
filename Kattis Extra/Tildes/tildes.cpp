786a01d1abf97454d6ffea0c57af77028bd8cc4b
BrendanCheong <brendancej82@gmail.com>
2022-03-10 13:33:28 +0800

    tildes completed using union find

---
+++ Kattis Extra/Tildes/tildes.cpp
@@ -0,0 +1,73 @@
+#include <bits/extc++.h>
+
+using namespace std;
+using namespace __gnu_pbds;
+using ll = long long;
+using ull = unsigned long long int;
+template <class T> using binary_tree = tree<T, null_type, logical_and<T>, rb_tree_tag, tree_order_statistics_node_update>;
+
+template <class T>
+class UnionFind {                                         // OOP style
+private:
+    vector<T> p, rank, setSize;                           // vll p is the key part
+    T numSets;
+public:
+    UnionFind(T N) {
+        p.assign(N, 0); for (size_t i = 0; i < N; ++i) p[i] = i; //? initially, parent of myself is myself, so p[i] = i
+        rank.assign(N, 0);                           // optional speedup
+        setSize.assign(N, 1);                        // optional feature
+        numSets = N;                                 // optional feature
+    }
+
+    T findSet(T i) {
+        return (p[i] == i)            //? if we have found the root, output the root, else continue to flatten tree until we have found root
+            ? i
+            : (p[i] = findSet(p[i])); // !flattens the tree so future reads are O(1) time instead O(log N)
+    }
+    bool isSameSet(T i, T j) {
+        return findSet(i) == findSet(j);
+    }
+
+    T numDisjointSets() {
+        return numSets;
+    }      // optional
+    T sizeOfSet(T i) {
+        return setSize[findSet(i)];
+    } // optional
+
+    void unionSet(T i, T j) {
+        if (isSameSet(i, j))
+            return;               // i and j are in same set
+        T x = findSet(i);
+        T y = findSet(j);         // find both rep items
+        if (rank[x] > rank[y])
+            swap(x, y);           //? keep x 'shorter' than y, x is always shorter tree compared to y
+        p[x] = y;                 //! set x under y, this combines two big subtrees into one tree
+        if (rank[x] == rank[y])
+            ++rank[y];            //? optional speedup, if the rank is actually the same, create a tree is that is one height taller, augmenting DS
+        setSize[y] += setSize[x]; // combine set sizes at y
+        --numSets;                // a union reduces numSets
+    }
+};
+
+int main()
+{
+    ll N, Q; cin >> N >> Q;
+    UnionFind<ll> UF(N);
+    for (size_t i = 0; i < Q; i++) {
+        char cmd; cin >> cmd;
+        switch (cmd) {
+        case('t'):
+            ll i, j; cin >> i >> j; // we must join these people
+            --i; --j; // make it zero based as a result of using vectors in UF
+            UF.unionSet(i, j);
+            break;
+        case('s'):
+            ll k; cin >> k; // we must find the set of this person
+            cout << UF.sizeOfSet(k - 1) << '\n'; // make sure its zero based
+            break;
+        }
+    }
+
+    return 0;
+}
