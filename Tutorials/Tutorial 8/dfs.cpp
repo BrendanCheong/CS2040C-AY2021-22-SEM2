eccd8b5e41c2a897e053c228a3e52436b98d1f72
BrendanCheong <brendancej82@gmail.com>
2022-03-25 10:32:52 +0800

    tutorial 8

--- Tutorials/Tutorial 8/dfs.cpp
+++ Tutorials/Tutorial 8/dfs.cpp
@@ -24,51 +24,49 @@ int main() {
     4 8    8 9    5 10   6 11   7 12   9 10   10 11  11 12
     */
 
-    // freopen("bfs_in.txt", "r", stdin);
-    freopen("dfs_cc_in.txt", "r", stdin);
+    freopen("bfs_in.txt", "r", stdin);
+    // freopen("dfs_cc_in.txt", "r", stdin);
 
-    // !uncomment for bfs.txt
-    // int V, E; scanf("%d %d", &V, &E);
-    // vector<vii> AL(V, vii());
-    // for (int i = 0; i < E; ++i) {
-    //     int a, b; scanf("%d %d", &a, &b);
-    //     AL[a].emplace_back(b, 0);
-    //     AL[b].emplace_back(a, 0);
-    // }
-
-    // !uncomment for dfs_cc.txt
-    int V; scanf("%d", &V);
+    int V, E; scanf("%d %d", &V, &E);
     vector<vii> AL(V, vii());
-    AL.assign(V, vii());
-    for (int u = 0; u < V; ++u) {
-        int k; scanf("%d", &k);
-        while (k--) {
-            int v, w; scanf("%d %d", &v, &w);
-            AL[u].emplace_back(v, w);
-        }
+    for (int i = 0; i < E; ++i) {
+        int a, b; scanf("%d %d", &a, &b);
+        AL[a].emplace_back(b, 0);
+        AL[b].emplace_back(a, 0);
     }
 
-    // as an example, we start from this source, see Figure 4.3
-    int s = 0;
+    // int V; scanf("%d", &V);
+    // vector<vii> AL(V, vii());
+    // AL.assign(V, vii());
+    // for (int u = 0; u < V; ++u) {
+    //     int k; scanf("%d", &k);
+    //     while (k--) {
+    //         int v, w; scanf("%d %d", &v, &w);
+    //         AL[u].emplace_back(v, w);
+    //     }
+    // }
 
-    // BFS routine inside int main() -- we do not use recursion
+    // as an example, we start from this source, see Figure 4.3
+    int s = 5;
+    cout << "=============== DFS ITERATIVE =================" << endl;
+
     vi dist(V, INF); dist[s] = 0;                  // INF = 1e9 here
-    queue<int> q; q.push(s);                       // start BFS from s
+    stack<int> stack; stack.push(s);                       // start BFS from s
     p.assign(V, -1);                               // p is global
 
     int layer = -1;                                // for output printing
     bool isBipartite = true;                       // additional feature
-    cout << "============== BFS ITERATIVE =================" << endl;
-    while (!q.empty()) {
-        int u = q.front(); q.pop();
-        if (dist[u] != layer) printf("\nLayer %d: ", dist[u]);
+
+    while (!stack.empty()) {
+        int u = stack.top(); stack.pop();
+        // if (dist[u] != layer) printf("\nLayer %d: ", dist[u]);
+        printf("visit %d \n", u);
         layer = dist[u];
-        printf("visit %d, ", u);
         for (auto& [v, w] : AL[u]) {                 // C++17 style, w ignored
             if (dist[v] == INF) {
                 dist[v] = dist[u] + 1;                     // dist[v] != INF now, we have visited the vertex
                 p[v] = u;                                // parent of v is u
-                q.push(v);                               // for next iteration
+                stack.push(v);                               // for next iteration
             }
             else if ((dist[v] % 2) == (dist[u] % 2))       // same parity
                 isBipartite = false;
