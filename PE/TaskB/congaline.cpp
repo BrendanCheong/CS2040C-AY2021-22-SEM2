cfc212ee1c178b5b705477202bf44aa85dd8c477
BrendanCheong <brendancej82@gmail.com>
2022-03-30 17:53:05 +0800

    completed task B LFGGGG

--- PE/TaskB/congaline.cpp
+++ PE/TaskB/congaline.cpp
@@ -42,13 +42,22 @@ int main()
     ll n, q; cin >> n >> q;
     list<string> conga;
     unordered_map<string, string> partners;
+    // Obviously we use a DLL for this, similarly this qns relies on a "Circular LL"
+    // I need to keep track of all the iterators of the people in the conga line
+    // if 'C' is cast, then I will insert behind the partner based on the iterator
+    // but how do I update the iterator? if A partner B, then B A, how do I update A iterator
+    // Ans: each dancer knows their partners iterator
+    // after inserting, update the dancer's partner iterator
+    unordered_map<string, list<string>::iterator> track;
     string p1, p2;
     for (size_t i = 0; i < n; i++) {
         cin >> p1 >> p2;
         conga.push_back(p1);
-        conga.push_back(p2);
         partners[p1] = p2;
+        track[p2] = --conga.end(); // each dancer knows the partners iterator
+        conga.push_back(p2);
         partners[p2] = p1;
+        track[p1] = --conga.end(); // same here, have to wait until we push_back to list
     }
     char input;
     auto it = conga.begin();
@@ -63,31 +72,33 @@ int main()
         case('F'):
             --it;
             break;
-        case('R'): // gg linear O(n) move
+        case('R'): // linear O(1) move since the input is an iterator
             auto original_it = it;
             string element = *original_it;
             ++it;
             if (it == conga.end()) it = conga.begin();
-            conga.erase(original_it); // O(n)
+            conga.erase(original_it); // O(1)
             conga.push_back(element);
             break;
+        }
+        if (input == 'C') {
+            auto original_it = it;
+            string current_dancer = *original_it;
+            ++it;
+            if (it == conga.end()) it = conga.begin();
+            // insert at my partners given original_it
+            conga.erase(original_it); // O(1)
+            auto partner_it = track[current_dancer];
+            auto new_partner_it = conga.insert(next(partner_it), current_dancer);
+            // update my dancer's partner iterator with the new inserted iterator 
+            // that belongs to the dancer
+            track[partners[current_dancer]] = new_partner_it;
         }
     }
     cout << '\n';
     for (auto& k : conga) {
         cout << k << '\n';
     }
-
-    // unordered_set<string> players;
-    // for (auto& k : conga) {
-    //     players.insert(k);
-    //     if (players.count(partners[k])) { // if I can find my partner!
-    //         cout << partners[k] << '\n';
-    //         cout << k << '\n';
-    //         players.erase(k);
-    //         players.erase(partners[k]);
-    //     }
-    // }
 
     return 0;
 }
