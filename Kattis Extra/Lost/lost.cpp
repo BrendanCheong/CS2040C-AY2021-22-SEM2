70fa9f17b175286a3287465d42cfa0120743eaf4
BrendanCheong <brendancej82@gmail.com>
2022-04-16 11:32:54 +0800

    completed lost

--- Kattis Extra/Lost/lost.cpp
+++ Kattis Extra/Lost/lost.cpp
@@ -39,32 +39,6 @@ void printVectorOfVectors(vector<vll>& v) {
     cout << '\n';
 }
 
-class UnionFind {                                // OOP style
-private:
-    vll p, rank, setSize;                           // vi p is the key part
-    ll numSets;
-public:
-    UnionFind(ll N) {
-        p.assign(N, 0); for (ll i = 0; i < N; ++i) p[i] = i;
-        rank.assign(N, 0);                           // optional speedup
-        setSize.assign(N, 1);                        // optional feature
-        numSets = N;                                 // optional feature
-    }
-    ll findSet(ll i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
-    bool isSameSet(ll i, ll j) { return findSet(i) == findSet(j); }
-    void unionSet(ll i, ll j) {
-        if (isSameSet(i, j)) return;                 // i and j are in same set
-        ll x = findSet(i), y = findSet(j);          // find both rep items
-        if (rank[x] > rank[y]) swap(x, y);           // keep x 'shorter' than y
-        p[x] = y;                                    // set x under y
-        if (rank[x] == rank[y]) ++rank[y];           // optional speedup
-        setSize[y] += setSize[x];                    // combine set sizes at y
-        --numSets;                                   // a union reduces numSets
-    }
-    ll numDisjointSets() { return numSets; }
-    ll sizeOfSet(ll i) { return setSize[findSet(i)]; }
-};
-
 int main()
 {
     // freopen("in.txt", "r", stdin);
@@ -86,64 +60,63 @@ int main()
         string l1, l2; cin >> l1 >> l2;
         ll w; cin >> w;
         AL[languages[l1]].push_back({ languages[l2], w });
-        // AL[languages[l2]].push_back({ languages[l1], w });
+        AL[languages[l2]].push_back({ languages[l1], w });
     }
 
     // since this qns uses lowest number of hops, followed by lowest weight of edge, its a modified BFS algo
     // in order to find the number of layers for each edge
     queue<ll> qu; qu.push(0);
-    vll dist(n + 1, INF); dist[0] = 0;
-    ll layer = -1;
+    vector<pll> dist(n + 1, { INF, INF });
+    dist[0] = { 0, 0 };
     while (qu.size() > 0) { // O(V+E)
         ll u = qu.front(); qu.pop();
-        layer = dist[u];
-        for (auto& [v, w] : AL[u]) {
-            // printf("vertex %llu is visiting vertex %llu of weight %llu, in layer %llu\n", u, v, w, layer + 1);
-            EL.push_back({ layer + 1, w, u, v });
-            if (dist[v] == INF) {
-                dist[v] = dist[u] + 1;
-                // cout << "hops from " << u << " to " << v << ": " << dist[v] << '\n';
+        auto [layer, distance] = dist[u]; // hold 2 kinds of distance, hops from the source and actual edge weight
+        for (auto& p : AL[u]) {
+            auto& [v, w] = p;
+            auto& [layer2, distance2] = dist[v];
+
+            // If we can get to a node with less translations
+            // we find the edge with the lowest amount of layers or hops from the source
+            // each vertex has its weight
+            // layer always starts of at 0
+            // layer + 1 is like your BFS relaxation, dist[u] + 1
+            if (layer + 1 < layer2) {
+                dist[v] = { layer + 1, w }; // we update the current distance with <number of smallest hops, weight of u->v>
                 qu.push(v);
+            }
+            // If we can get to a node with the same translations and less money
+            // pick the least weighted incoming edge
+            // for example, vertex "Swedish" has either (A to S as 5) or (F to S as 1)
+            // we compare vertex u -> v weight vs previous weight of previous vertexes u2 -> v weight and pick the smallest one  
+            else if (layer + 1 == layer2) {
+                dist[v].second = min(w, distance2); // take the smallest weighted edge
             }
         }
     }
-    // comparator for EL
-    // pick the smallest first index first
-    // if identical, pick the smallest second index
 
-    auto comp = [](llll& e1, llll& e2) {
-        if (get<0>(e1) == get<0>(e2)) {
-            return get<1>(e1) < get<1>(e2);
+    // print dist vector
+    ll ans = 0;
+    for (auto& [k, v] : dist) {
+        if (v == INF) {
+            cout << "Impossible\n";
+            return 0;
         }
-        return get<0>(e1) < get<0>(e2);
-    };
-
-    sort(EL.begin(), EL.end(), comp); // O(E log E)
-
-    // print EL
-    // for (auto& e : EL)
-    //     cout << get<0>(e) << " " << get<1>(e) << " " << get<2>(e) << " " << get<3>(e) << '\n';
-    // printVector(dist);
-
-    ll mst_cost = 0, num_taken = 0;               // no edge has been taken
-    UnionFind UF(n + 1);                               // all V are disjoint sets
-    // note: the runtime cost of UFDS is very light
-    for (ll i = 0; i < m; ++i) {                  // up to O(E)
-        auto [l, w, u, v] = EL[i];                      // C++17 style
-        if (UF.isSameSet(u, v)) continue;            // already in the same CC
-        mst_cost += w;                               // add w of this edge
-        UF.unionSet(u, v);                           // link them
-        ++num_taken;                                 // 1 more edge is taken
-        if (num_taken == n) break;                 // optimization
+        ans += v;
     }
-    // note: the number of disjoint sets must eventually be 1 for a valid MST
-    // total time: O(E log V)
-    if (num_taken != n)
-        cout << "Impossible" << '\n';
-    else
-        cout << mst_cost << '\n';
 
-    // total Time complexity: O(E) + O(V) + O(V+E) + O(E log E) + O(E log V) = O(E log E) in this case as E > V
+    cout << ans << '\n';
+
+    /**
+     * Pseudo Code:
+     * We will decide to use BFS in this case as we need to count the number of hops
+     * Since we will use BFS, we can modify it such that when we relax the vertices, we can not only count the number of hops
+     * But we can also make sure the vertice stores the SMALLEST WEIGHTED incoming edge to that vertex
+     * Thus, our distance array stores index of vertex: <number of hops, smallest incoming edge weight>
+     * We first detect if number of hops is < current number hops whe u -> v (starts off as 0 + 1 < INF)
+     * If number of hops are the same, take the min of the weights of the incoming edges
+     * thus the relaxation function is like this := distance[v] = { distance[u].first + 1 (first is the number of hops), min(distance[u].second, w) second is the weight }
+     * Take away is that BFS can store additional information in each vertex, especially for incoming edges AND number of hops
+    */
 
     return 0;
 }
